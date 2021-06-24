6720c07cdf7c3e812e0d539672c0adaa85242e95
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2021-06-24 11:57:31 -0300

    Primeiro teste JUnit

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -29,7 +32,8 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void Teste() {
 		
 		//Cenário
 		LocacaoService service = new LocacaoService();
@@ -40,9 +44,9 @@ public class LocacaoService {
 	    Locacao locacao = service.alugarFilme(usuario,filme);
 		
 		//Verificação
-	    System.out.println(locacao.getValor() == 3.5);
-	    System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-	    System.out.println(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), locacao.getDataRetorno()));
+	    Assert.assertTrue(locacao.getValor() ==3.5);
+	    Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+	    Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), locacao.getDataRetorno()));
 		
 		
 	}
