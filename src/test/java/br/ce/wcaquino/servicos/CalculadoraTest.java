1cfe826aae72cfd48093fe1b153d346165977a39
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2021-06-29 16:12:47 -0300

    tdd - primeira parte

---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,73 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+	
+	private Calculadora calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+	}
+	
+	@Test
+	public void deveSomarDoisValores() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		//Calculadora calc = new Calculadora();
+		
+		//acao
+		int resultado = calc.somar(a, b);
+		
+		//verificacao
+		Assert.assertEquals(8, resultado);
+	}
+	
+	@Test
+	public void deveSubtrairDoisValores() {
+		//cenario
+		int a = 7;
+		int b = 3;
+		//Calculadora calc = new Calculadora();
+		
+		//acao
+		int resultado = calc.subtrair(a, b);
+		
+		//verificacao
+		Assert.assertEquals(4, resultado);
+	}
+	
+	@Test
+	public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		//cenario
+		int a = 6;
+		int b = 3;
+		//Calculadora calc = new Calculadora();
+		
+		//acao
+		int resultado = calc.dividir(a, b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultado);
+	}
+	
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void develancarExcecaoAoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		//cenario
+		int a = 10;
+		int b = 0;
+		//Calculadora calc = new Calculadora();
+		
+		//acao
+		calc.dividir(a, b);
+
+	}
+		
+
+}
